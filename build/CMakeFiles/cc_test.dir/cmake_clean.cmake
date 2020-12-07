913db08728a6c0b563201a300b2720b3679061e4
haoge2001 <934622645@qq.com>
2020-12-07 21:53:47 +0800

    2020-12-7

---
+++ build/CMakeFiles/cc_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cc_test.dir/main.cpp.o"
+  "cc_test"
+  "cc_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cc_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
